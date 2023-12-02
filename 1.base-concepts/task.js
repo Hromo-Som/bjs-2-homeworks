0294e402bb54da666bb9255e7fad7b03ea949b8c
HromoSom <temur.haydarov@gmail.com>
2023-12-02 11:29:29 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,29 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b ** 2 - 4 * a * c;
-  
-  if (d < 0) {
-    return arr;
-  } else if (d === 0) {
-    arr.push(- b / (2 * a));
-  } else {
-    arr.push((- b + Math.sqrt(d) ) / (2 * a));
-    arr.push((- b - Math.sqrt(d) ) / (2 * a));
-  };
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
 
-  return arr;
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	};
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (typeof(percent) !== 'number' || typeof(contribution) !== 'number' || typeof(amount) !== 'number' || typeof(countMonths) !== 'number') {
-    return false;
-  };
+	if (typeof(percent) !== 'number' || typeof(contribution) !== 'number' || typeof(amount) !== 'number' || typeof(countMonths) !== 'number') {
+		return false;
+	};
 
-  let p = percent / 100 / 12;
-  let s = amount - contribution;
-  let monthlyPayment = s * (p + (p / (((1 + p)**countMonths) - 1)));
-  let totalAmount = monthlyPayment * countMonths;
-  return +totalAmount.toFixed(2);
+	let p = percent / 100 / 12;
+	let s = amount - contribution;
+	let monthlyPayment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let totalAmount = monthlyPayment * countMonths;
+	return +totalAmount.toFixed(2);
 }
