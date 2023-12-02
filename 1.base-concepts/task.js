c56fcfe6bc7971495fb3c53398996ccaea855140
HromoSom <temur.haydarov@gmail.com>
2023-12-02 11:24:20 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,4 +1,5 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
@@ -16,5 +17,13 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (typeof(percent) !== 'number' || typeof(contribution) !== 'number' || typeof(amount) !== 'number' || typeof(countMonths) !== 'number') {
+    return false;
+  };
+
+  let p = percent / 100 / 12;
+  let s = amount - contribution;
+  let monthlyPayment = s * (p + (p / (((1 + p)**countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return +totalAmount.toFixed(2);
 }
